43697791b90000757e35f4bb7b8fe10f9cd7ec28
Abdoulllahi <dabdoulahi@gmail.com>
2024-01-02 20:43:05 -0600

    maxArea

--- Algos/src/algos/leetcode75/two_pointers/MoveZeros.java
+++ Algos/src/algos/leetcode75/two_pointers/MoveZeros.java
@@ -1,16 +1,26 @@
 package algos.leetcode75.two_pointers;
 
+import java.util.Arrays;
+
 public class MoveZeros {
 
-    public void moveZeros(int[] num) {
+    public static void moveZeros(int[] nums) {
         int i = 0;
-        for (int j = 0; j < num.length; j++) {
-            if (num[j] != 0) {
-                int temp = num[i];
-                num[i] = num[j];
-                num[j] = temp;
-                i++;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != 0) {
+            if ( i != j) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
             }
+            i++;
         }
+        }
+        System.out.println(Arrays.toString(nums));
+    }
+
+    public static void main(String[] args) {
+        int[] myArray = new int[] {1, 0, 2, 0, 3};
+        moveZeros(myArray);
     }
 }
